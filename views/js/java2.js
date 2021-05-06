d53e28ecaac6882cb549241586baff4667e50b4a
Damon Coffey <damoncoffey@my.uri.edu>
2021-05-06 19:01:09 -0400

    Fixed review page

--- views/js/java2.js
+++ views/js/java2.js
@@ -1,59 +1,59 @@
 $(document).ready(function() {
 
-    $("p").hide();
-   
-    $("#pic1").hover(function() {
-      $("#p1").show();
-    }, function() {
-      $("#p1").hide();
-    });
-    
-      $("#pic2").hover(function() {
-      $("#p2").show();
-    }, function() {
-      $("#p2").hide();
-    });
-    
-      $("#pic3").hover(function() {
-      $("#p3").show();
-    }, function() {
-      $("#p3").hide();
-    });
-    
-     $("#pic4").hover(function() {
-      $("#p4").show();
-    }, function() {
-      $("#p4").hide();
-    });
-    
-     $("#pic5").hover(function() {
-      $("#p5").show();
-    }, function() {
-      $("#p5").hide();
-    });
-    
-     $("#pic6").hover(function() {
-      $("#p6").show();
-    }, function() {
-      $("#p6").hide();
-    });
-    
-     $("#pic7").hover(function() {
-      $("#p7").show();
-    }, function() {
-      $("#p7").hide();
-    });
-    
-     $("#pic8").hover(function() {
-      $("#p8").show();
-    }, function() {
-      $("#p8").hide();
-    });
-    
-     $("#pic9").hover(function() {
-      $("#p9").show();
-    }, function() {
-      $("#p9").hide();
-    });
+  $("p").hide();
+ 
+  $("#pic1").hover(function() {
+    $("#p1").show();
+  }, function() {
+    $("#p1").hide();
   });
+  
+    $("#pic2").hover(function() {
+    $("#p2").show();
+  }, function() {
+    $("#p2").hide();
+  });
+  
+    $("#pic3").hover(function() {
+    $("#p3").show();
+  }, function() {
+    $("#p3").hide();
+  });
+  
+   $("#pic4").hover(function() {
+    $("#p4").show();
+  }, function() {
+    $("#p4").hide();
+  });
+  
+   $("#pic5").hover(function() {
+    $("#p5").show();
+  }, function() {
+    $("#p5").hide();
+  });
+  
+   $("#pic6").hover(function() {
+    $("#p6").show();
+  }, function() {
+    $("#p6").hide();
+  });
+  
+   $("#pic7").hover(function() {
+    $("#p7").show();
+  }, function() {
+    $("#p7").hide();
+  });
+  
+   $("#pic8").hover(function() {
+    $("#p8").show();
+  }, function() {
+    $("#p8").hide();
+  });
+  
+   $("#pic9").hover(function() {
+    $("#p9").show();
+  }, function() {
+    $("#p9").hide();
+  });
+});
   
